1d1c7649260d159c2d25f2167172de66b349b162
shota mochizuki <asahina@shota-no-MacBook-Pro.local>
2019-05-10 12:59:49 +0900

    Ajax message send

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,60 @@
+$(function(){
+  function buildHTML(message){
+    var html;
+    if( message.image == null) {
+      html = `<div class='message'>
+                  <div class='upper-info'>
+                    <p class='upper-info__user'>
+                      ${message.user_name}
+                    </p>
+                    <p class='upper-info__date'>
+                      ${message.created_at}
+                    </p>
+                  </div>
+                  <p class='message__text'>
+                    ${message.body}
+                  </p>
+                </div>`
+    } else {
+      html = `<div class='message'>
+                  <div class='upper-info'>
+                    <p class='upper-info__user'>
+                      ${message.user_name}
+                    </p>
+                    <p class='upper-info__date'>
+                      ${message.created_at}
+                    </p>
+                  </div>
+                  <p class='message__text'>
+                    ${message.body}
+                  </p>
+                  <img class="lower-message__image" src="${message.image}" />
+                </div>`
+    }
+    return html;
+  }
+  $(document).on('submit','#new_message', function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.input-box__text').val('')
+      $('.image-label__input').val('')
+      $(".new-message__submit-btn").attr('disabled', false);
+      $('.messages').animate({scrollTop: $('.messages').get(0).scrollHeight});
+      $('.messages').append(html)
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+})
