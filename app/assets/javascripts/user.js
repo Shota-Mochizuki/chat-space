6087f00dd783a247ea9ae0a01687aca9fc0d28d6
shota mochizuki <asahina@shota-no-MacBook-Pro.local>
2019-05-18 17:39:52 +0900

    Modify group function

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -8,7 +8,7 @@ $(function(){
   }
   function addMember(name,id){
     var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-                  <input name='group[user_ids][]' type='hidden' value='${id}'>
+                  <input name='group[user_ids][]' type='hidden' value='${id}' class='chat-member'>
                   <p class='chat-group-user__name'>${name}</p>
                   <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
                 </div>`
@@ -24,10 +24,28 @@ $(function(){
       dataType: 'json',
     })
     .done(function(users){
+      var chat_member = [];
+      var j = 0;
+      $(".chat-member").each(function(i){
+        chat_member[i] = $(this).attr('value');
+      });
       $(".user-search-result").empty();
       if ((input !== '') && (users.length !== 0)) {
         users.forEach(function(user){
-          $('.user-search-result').append(addResult(user));
+          if(chat_member.length == 0){
+            $('.user-search-result').append(addResult(user));
+          } else{
+            while(1){
+              if(user.id == chat_member[j]){
+                break;
+              }
+              j++;
+              if(j == chat_member.length){
+                $('.user-search-result').append(addResult(user));
+                break;
+              }
+            }
+          }
         });
       }
     })
